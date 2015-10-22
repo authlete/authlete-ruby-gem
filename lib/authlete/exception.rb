b5cf26101d99017a73583499908c6a7e06d48f78
hidebike712 <hidebike712@hidebike712-no-MacBook-Air.local>
2015-10-23 00:05:22 +0900

    Fixed a bug in the exception class.

--- lib/authlete/exception.rb
+++ lib/authlete/exception.rb
@@ -46,8 +46,8 @@ module Authlete
     end
 
     def to_s
-      super + " { status_code:'#{@status_code}', message:'#{@message}',"
-            + " result_code:'#{@result_code}', result_message:'#{@result_message}' }"
+      "#{self.name} => { status_code:'#{@status_code}', message:'#{@message}'," +
+      " result_code:'#{@result_code}', result_message:'#{@result_message}' }"
     end
   end
 end
