a6eb04fc2468221de0879fab3afba40d87727287
Takahiko Kawasaki <taka@authlete.com>
2020-11-10 12:57:07 +0900

    Added a mechanism for testing.

--- Rakefile
+++ Rakefile
@@ -1,2 +1,7 @@
 require "bundler/gem_tasks"
+require "rake/testtask"
 
+# Add "test" task
+Rake::TestTask.new do |t|
+  t.pattern = 'test/**/test_*.rb'
+end
---
+++ test/authlete/model/response/test_authorization-response.rb
@@ -0,0 +1,137 @@
+# :nodoc:
+#
+# Copyright (C) 2020 Authlete, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+require 'authlete'
+require 'json'
+require 'minitest/autorun'
+
+
+class AuthorizationResponseTest < Minitest::Test
+  ACRS                   = ["acr0", "acr1"]
+  ACTION                 = "FORM"
+  CLAIMS_LOCALES         = ["en", "ja"]
+  CLAIMS                 = ["claim0", "claim1"]
+  DISPLAY                = "PAGE"
+  ID_TOKEN_CLAIMS        = "<id-token-claims>"
+  LOGIN_HINT             = "<login-hint>"
+  MAX_AGE                = 100
+  PROMPTS                = ["CONSENT", "LOGIN"]
+  PURPOSE                = "<purpose>"
+  REQUEST_OBJECT_PAYLOAD = "<request-object-payload>"
+  RESOURCES              = ["resource0", "resource1"]
+  RESPONSE_CONTENT       = "<response-content>"
+  SERVICE_NAME           = "<service-name>"
+  SERVICE                = Authlete::Model::Service.new({"serviceName":SERVICE_NAME})
+  SCOPE_NAME             = "scope0"
+  SCOPE_DESCRIPTION      = "<scope0-description>"
+  SCOPES                 = [Authlete::Model::Scope.new({"name":SCOPE_NAME,"description":SCOPE_DESCRIPTION})]
+  SUBJECT                = "<subject>"
+  TICKET                 = "<ticket>"
+  UI_LOCALES             = ["de", "fr"]
+  USER_INFO_CLAIMS       = "<user-info-claims>"
+
+
+  def generate_json
+    return <<~JSON
+      {
+        "acrs":                 ["acr0", "acr1"],
+        "action":               "FORM",
+        "claimsLocales":        ["en", "ja"],
+        "claims":               ["claim0", "claim1"],
+        "display":              "PAGE",
+        "idTokenClaims":        "<id-token-claims>",
+        "loginHint":            "<login-hint>",
+        "maxAge":               100,
+        "prompts":              ["CONSENT", "LOGIN"],
+        "purpose":              "<purpose>",
+        "requestObjectPayload": "<request-object-payload>",
+        "resources":            ["resource0", "resource1"],
+        "responseContent":      "<response-content>",
+        "service": {
+            "serviceName":      "<service-name>"
+        },
+        "scopes": [
+            {
+                "name":         "scope0",
+                "description":  "<scope0-description>"
+            }
+        ],
+        "subject":              "<subject>",
+        "ticket":               "<ticket>",
+        "uiLocales":            ["de", "fr"],
+        "userInfoClaims":       "<user-info-claims>"
+      }
+    JSON
+  end
+
+
+  def match(obj)
+    assert_equal ACRS,                   obj.acrs
+    assert_equal ACTION,                 obj.action
+    assert_equal CLAIMS_LOCALES,         obj.claimsLocales
+    assert_equal CLAIMS,                 obj.claims
+    assert_equal DISPLAY,                obj.display
+    assert_equal ID_TOKEN_CLAIMS,        obj.idTokenClaims
+    assert_equal MAX_AGE,                obj.maxAge
+    assert_equal PROMPTS,                obj.prompts
+    assert_equal PURPOSE,                obj.purpose
+    assert_equal REQUEST_OBJECT_PAYLOAD, obj.requestObjectPayload
+    assert_equal RESOURCES,              obj.resources
+    assert_equal RESPONSE_CONTENT,       obj.responseContent
+    assert_equal SERVICE_NAME,           obj.service.serviceName
+    assert_equal SCOPE_NAME,             obj.scopes[0].name
+    assert_equal SCOPE_DESCRIPTION,      obj.scopes[0].description
+    assert_equal SUBJECT,                obj.subject
+    assert_equal TICKET,                 obj.ticket
+    assert_equal UI_LOCALES,             obj.uiLocales
+    assert_equal USER_INFO_CLAIMS,       obj.userInfoClaims
+  end
+
+
+  def test_from_json
+    jsn = generate_json()
+    hsh = JSON.parse(jsn)
+    obj = Authlete::Model::Response::AuthorizationResponse.new(hsh)
+
+    match(obj)
+  end
+
+
+  def test_setters
+    obj = Authlete::Model::Response::AuthorizationResponse.new
+    obj.acrs                 = ACRS
+    obj.action               = ACTION
+    obj.claimsLocales        = CLAIMS_LOCALES
+    obj.claims               = CLAIMS
+    obj.display              = DISPLAY
+    obj.idTokenClaims        = ID_TOKEN_CLAIMS
+    obj.maxAge               = MAX_AGE
+    obj.prompts              = PROMPTS
+    obj.purpose              = PURPOSE
+    obj.requestObjectPayload = REQUEST_OBJECT_PAYLOAD
+    obj.resources            = RESOURCES
+    obj.responseContent      = RESPONSE_CONTENT
+    obj.service              = SERVICE
+    obj.scopes               = SCOPES
+    obj.subject              = SUBJECT
+    obj.ticket               = TICKET
+    obj.uiLocales            = UI_LOCALES
+    obj.userInfoClaims       = USER_INFO_CLAIMS
+
+    match(obj)
+  end
+end
